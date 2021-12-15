fd4bcc4c2cdf46687ec060d6418a0e7a52cb85bd
Jason Gutierrez <jasong84@gmail.com>
2021-12-14 22:56:02 -0600

    Create store and update formField value in component

--- src/lib/components/formField.svelte
+++ src/lib/components/formField.svelte
@@ -1,13 +1,18 @@
 <script>
 	export let fieldTitle;
 	export let fieldName;
-	export let fieldValue;
+	let fieldValue;
+	let colorValue;
+
+	function handleColorChange(color){
+		fieldValue = color;
+	}
 </script>
 
 <div class="flex flex-col gap-x-2 items-start">
 	<label class="text-gray-800 font-semibold block my-3 text-sm" for="rootOrange">{fieldTitle}</label>
 	<div class="flex">
-		<input type="color" class="w-14 h-8 p-0 focus:outline-none" bind:value={fieldValue} />
+		<input type="color" class="w-14 h-8 p-0 focus:outline-none" bind:value={colorValue} on:change={handleColorChange(colorValue)}  />
 		<input type="text" class="w-20 border px-2 focus:outline-none" name={fieldName} id={fieldName} bind:value={fieldValue}>
 	</div>
 </div>

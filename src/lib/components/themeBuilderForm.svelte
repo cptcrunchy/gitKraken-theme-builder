82800bb326525706054e50c7b0274b084eb6009a
Jason Gutierrez <jasong84@gmail.com>
2021-12-15 21:59:38 -0600

    Update form Field components in form and use theme builder object

--- src/lib/components/themeBuilderForm.svelte
+++ src/lib/components/themeBuilderForm.svelte
@@ -1,21 +1,29 @@
 <script>
+	import { gkTheme } from '$lib/theme';
 	import FormField from '$lib/components/formField.svelte'
 	let isChecked = false;
-	let fieldValue = "";
-	$:console.log()
+	
+	function toggleTheme(){
+		isChecked = !isChecked;
+		gkTheme.meta.scheme = isChecked ? "dark" : 'light';
+	}
+
+	$:console.dir(gkTheme.themeValues.root)
 </script>
 
 <form class="bg-white w-full lg:w-11/12">
 
 	<label class="text-gray-800 font-semibold block my-3 text-md" for="themeName">Theme Name</label>
-	<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" id="username" placeholder="GitKraken Dark" />	
+	<input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+		type="text" name="themeName" id="themeName"
+		placeholder="GitKraken Dark" bind:value={gkTheme.meta.name} />	
 	<label class="text-gray-800 font-semibold block my-3 text-md" for="themeType">Theme Type</label>
 	<div for="themeType" class="mt-3 inline-flex items-center cursor-pointer">
 		<span class="relative">
 			<span class="block w-10 h-6 bg-gray-400 rounded-full shadow-inner"></span>
 			<span 
 				class="{isChecked ? "bg-purple-600 transform translate-x-full" : "bg-white"} absolute block w-4 h-4 mt-1 ml-1  rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out" >
-				<input id="checked" type="checkbox" class="absolute opacity-0 w-4 h-4" on:click={() => isChecked = !isChecked} />
+				<input id="checked" type="checkbox" class="absolute opacity-0 w-4 h-4" on:click={toggleTheme} />
 			</span>
 		</span>
 		<span class="ml-3 text-sm">{isChecked ? "Dark" : "Light"} Theme</span>
@@ -23,47 +31,43 @@
 	<h2 class="text-gray-800 font-semibold block my-3 text-lg">Theme Values</h2>
 	<h2 class="text-gray-800 font-semibold block my-3 text-md border-b-2">Root Values</h2>
 	<div class="flex gap-x-2 flex-wrap justify-start">
-		<FormField fieldTitle={"Red"} fieldName={"red"} bind:fieldValue={fieldValue} />
-		<FormField fieldTitle={"Orange"} fieldName={"orange"} />
-		<FormField fieldTitle={"Yellow"} fieldName={"yellow"} />
-		<FormField fieldTitle={"Green"} fieldName={"green"} />
-		<FormField fieldTitle={"Blue"} fieldName={"blue"} />
-		<FormField fieldTitle={"Light Blue"} fieldName={"ltblue"} />
-		<FormField fieldTitle={"App Bg"} fieldName={"appbg0"} />
-		<FormField fieldTitle={"Input Bg"} fieldName={"inputbg"} />
-		<FormField fieldTitle={"Input Bg Warning"} fieldName={"inputbgwarncolor"} />
-		<FormField fieldTitle={"Tool Bar Bg"} fieldName={"toolbarbg0"} />
-		<FormField fieldTitle={"Panel Bg"} fieldName={"panelbg0"} />
-		<FormField fieldTitle={"Panel Border"} fieldName={"panelborder"} />
-		<FormField fieldTitle={"Section Border"} fieldName={"sectionborder"} />
-		<FormField fieldTitle={"Subtle Border"} fieldName={"subtleborder"} />
-		<FormField fieldTitle={"Modal Overlay"} fieldName={"modaloverlay"} />
-		<FormField fieldTitle={"Text Color"} fieldName={"textcolor"} />
-		<FormField fieldTitle={"Text Accent"} fieldName={"textaccent"} />
-		<FormField fieldTitle={"Text Inverse"} fieldName={"textinverse"} />
-		<FormField fieldTitle={"Link Color"} fieldName={"linkcolor"} />
-		<FormField fieldTitle={"Terminal Repo Name"} fieldName={"termreponame"} />
-		<FormField fieldTitle={"Terminal Repo Branch"} fieldName={"termrepobranch"} />
-		<FormField fieldTitle={"Terminal Repo Tag"} fieldName={"termrepotag"} />
-		<FormField fieldTitle={"Terminal Repo Upstream"} fieldName={"termrepoupstream"} />
+		<FormField fieldTitle={"Red"} fieldName={"red"} bind:fieldValue={gkTheme.themeValues.root.red} />
+		<FormField fieldTitle={"Orange"} fieldName={"orange"} bind:fieldValue={gkTheme.themeValues.root.orange} />
+		<FormField fieldTitle={"Yellow"} fieldName={"yellow"} bind:fieldValue={gkTheme.themeValues.root.yellow} />
+		<FormField fieldTitle={"Green"} fieldName={"green"} bind:fieldValue={gkTheme.themeValues.root.green} />
+		<FormField fieldTitle={"Blue"} fieldName={"blue"} bind:fieldValue={gkTheme.themeValues.root.blue} />
+		<FormField fieldTitle={"Light Blue"} fieldName={"ltblue"} bind:fieldValue={gkTheme.themeValues.root.ltblue} />
+		<FormField fieldTitle={"App Bg"} fieldName={"appbg0"} bind:fieldValue={gkTheme.themeValues.root.app__bg0} />
+		<FormField fieldTitle={"Input Bg"} fieldName={"inputbg"} bind:fieldValue={gkTheme.themeValues.root.input__bg} />
+		<FormField fieldTitle={"Section Border"} fieldName={"sectionborder"} bind:fieldValue={gkTheme.themeValues.root.sectionBorder} />
+		<FormField fieldTitle={"Subtle Border"} fieldName={"subtleborder"} bind:fieldValue={gkTheme.themeValues.root.subtleBorder} />
+		<FormField fieldTitle={"Modal Overlay"} fieldName={"modaloverlay"} bind:fieldValue={gkTheme.themeValues.root.modalOverlay} />
+		<FormField fieldTitle={"Text Color"} fieldName={"textcolor"} bind:fieldValue={gkTheme.themeValues.root.textColor} />
+		<FormField fieldTitle={"Text Accent"} fieldName={"textaccent"} bind:fieldValue={gkTheme.themeValues.root.textAccent} />
+		<FormField fieldTitle={"Text Inverse"} fieldName={"textinverse"} bind:fieldValue={gkTheme.themeValues.root.textInverse} />
+		<FormField fieldTitle={"Link Color"} fieldName={"linkcolor"} bind:fieldValue={gkTheme.themeValues.root.linkColor} />
+		<FormField fieldTitle={"Terminal Repo Name"} fieldName={"termreponame"} bind:fieldValue={gkTheme.themeValues.root.termRepoName} />
+		<FormField fieldTitle={"Terminal Repo Branch"} fieldName={"termrepobranch"} bind:fieldValue={gkTheme.themeValues.root.termRepoBranch} />
+		<FormField fieldTitle={"Terminal Repo Tag"} fieldName={"termrepotag"} bind:fieldValue={gkTheme.themeValues.root.termRepoTag} />
+		<FormField fieldTitle={"Terminal Repo Upstream"} fieldName={"termrepoupstream"} bind:fieldValue={gkTheme.themeValues.root.termRepoUpstream} />
 	</div>
 	<h2 class="text-gray-800 font-semibold block my-3 text-md border-b-2">Tool bar</h2>
 	<div class="flex gap-x-2 flex-wrap justify-start">
-		<FormField fieldTitle={"Text Selected"} fieldName={"toolbartxtselected"} />
-		<FormField fieldTitle={"Text Normal"} fieldName={"toolbartxtnoraml"} />
-		<FormField fieldTitle={"Text Secondary"} fieldName={"toolbartxtsecondary"} />
-		<FormField fieldTitle={"Text Disabled"} fieldName={"toolbartxtdisabled"} />
-		<FormField fieldTitle={"Input Bg"} fieldName={"toolbarinputbg"} />
-		<FormField fieldTitle={"Link Color"} fieldName={"toolbarlinkcolor"} />
+		<FormField fieldTitle={"Text Selected"} fieldName={"toolbartxtselected"} bind:fieldValue={gkTheme.themeValues.toolbar.textSelected} />
+		<FormField fieldTitle={"Text Normal"} fieldName={"toolbartxtnoraml"} bind:fieldValue={gkTheme.themeValues.toolbar.textNormal} />
+		<FormField fieldTitle={"Text Secondary"} fieldName={"toolbartxtsecondary"} bind:fieldValue={gkTheme.themeValues.toolbar.textSecondary} />
+		<FormField fieldTitle={"Text Disabled"} fieldName={"toolbartxtdisabled"} bind:fieldValue={gkTheme.themeValues.toolbar.textDisabled} />
+		<FormField fieldTitle={"Input Bg"} fieldName={"toolbarinputbg"} bind:fieldValue={gkTheme.themeValues.toolbar.input__bg} />
+		<FormField fieldTitle={"Link Color"} fieldName={"toolbarlinkcolor"} bind:fieldValue={gkTheme.themeValues.toolbar.linkColor} />
 	</div>
 	<h2 class="text-gray-800 font-semibold block my-3 text-md border-b-2">Tabs bar</h2>
 	<div class="flex gap-x-2 flex-wrap justify-start">
-		<FormField fieldTitle={"Text Selected"} fieldName={"tabsbartxtselected"} />
-		<FormField fieldTitle={"Text Normal"} fieldName={"tabsbartxtnoraml"} />
-		<FormField fieldTitle={"Text Secondary"} fieldName={"tabsbartxtsecondary"} />
-		<FormField fieldTitle={"Text Disabled"} fieldName={"tabsbartxtdisabled"} />
-		<FormField fieldTitle={"Input Bg"} fieldName={"tabsbarinputbg"} />
-		<FormField fieldTitle={"Link Color"} fieldName={"tabsbarlinkcolor"} />
+		<FormField fieldTitle={"Text Selected"} fieldName={"tabsbartxtselected"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
+		<FormField fieldTitle={"Text Normal"} fieldName={"tabsbartxtnoraml"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
+		<FormField fieldTitle={"Text Secondary"} fieldName={"tabsbartxtsecondary"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
+		<FormField fieldTitle={"Text Disabled"} fieldName={"tabsbartxtdisabled"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
+		<FormField fieldTitle={"Input Bg"} fieldName={"tabsbarinputbg"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
+		<FormField fieldTitle={"Link Color"} fieldName={"tabsbarlinkcolor"} bind:fieldValue={gkTheme.themeValues.tabsbar.textSelected} />
 	</div>
 	<button type="submit" class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Create Theme</button>
 </form>
---
+++ src/lib/theme.js
@@ -0,0 +1,50 @@
+export let gkTheme = {
+	meta: {
+		name: '',
+		scheme: 'light'
+	},
+	themeValues: {
+		root: {
+			red: '',
+			orange: '',
+			yellow: '',
+			green: '',
+			blue: '',
+			ltblue: '',
+			app__bg0: '',
+			input__bg: '',
+			sectionBorder: '',
+			subtleBorder: '',
+			modalOverlay: '',
+			textColor: '',
+			textAccent: '',
+			textInverse: '',
+			linkColor: '',
+			cardShadow: '',
+			modifiedLine: '',
+			unverified: '',
+			termRepoName: '',
+			termRepoBranch: '',
+			termRepoTag: '',
+			termRepoUpstream: ''
+		},
+		toolbar: {
+			textSelected: '',
+			textNormal: '',
+			textSecondary: '',
+			textDisabled: '',
+			sectionBorder: '',
+			input__bg: '',
+			linkColor: ''
+		},
+		tabsbar: {
+			textSelected: '',
+			textNormal: '',
+			textSecondary: '',
+			textDisabled: '',
+			sectionBorder: '',
+			input__bg: '',
+			linkColor: ''
+		}
+	}
+};

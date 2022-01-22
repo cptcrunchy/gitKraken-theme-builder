74cea2b74dfd28b1daf851a898437dc5f32b825a
Jason Gutierrez <jasong84@gmail.com>
2022-01-22 14:02:30 -0600

    Update title of landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,6 @@
 <ThemeTitle />
 
 <div class="flex flex-col w-fit sm:w-full lg:w-1/2 sm:px-3 justify-center py-10 items-center bg-white overflow-auto">
-	<h1 class="text-gray-800 font-bold text-2xl mb-1">Build your GitKraken Theme!</h1>
+	<h1 class="text-gray-800 font-bold text-2xl mb-1">GitKraken Theme Builder!</h1>
 	<ThemeBuilderForm />
 </div>
